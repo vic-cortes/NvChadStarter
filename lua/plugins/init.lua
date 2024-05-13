23e6944b1ecb53e29be23b0f18b6ed48e2e32b01
Victor Cortes Figueroa <vmcortesf@gmail.com>
2024-05-12 20:21:26 -0500

    Added autoformatting on save config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,14 +17,20 @@ return {
   },
 
   {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier",
-        "pyright", "mypy", "ruff"
-  		},
-  	},
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "pyright",
+        "mypy",
+        "ruff",
+        "black",
+      },
+    },
   },
   --
   -- {
@@ -38,9 +44,28 @@ return {
   -- },
   {
     "jose-elias-alvarez/null-ls.nvim",
-    ft = {"python"},
+    ft = { "python" },
     opts = function()
       require "configs.null-ls"
     end,
-  }
+  },
+  -- Virtual environment selector
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    opts = function()
+      require "configs.venv-selector"
+    end,
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { "<leader>vs", "<cmd>VenvSelect<cr>" },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+    },
+  },
+  -- Indent
+  {
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+  },
 }
